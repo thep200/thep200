7dd6177348d9bf24e4c70ce23f6450b9d3f4b79e
thep200 <thep200.work@gmail.com>
2023-09-25 21:59:23 +0700

    add logic hero run

--- pages/js.js
+++ pages/js.js
@@ -1,14 +1,60 @@
-function whereIsHero(e, hero, places) {
+function heroRun(e, hero, places)
+{
+    const hero_current_coor = hero.getBoundingClientRect();
+
+    const hero_current_coor_x = hero_current_coor.left;
+    const hero_current_coor_y = hero_current_coor.top;
+
+    const hero_next_coor_x = e.clientX;
+    const hero_next_coor_y = e.clientY;
+
+    movePointToB(
+        e,
+        {x: hero_current_coor_x, y: hero_current_coor_y},
+        {x: hero_next_coor_x, y: hero_next_coor_y},
+        hero,
+        places
+    );
+}
+
+
+function movePointToB(e, pointA, pointB, hero, places)
+{
+    const duration = 1500;
+
+    const stepX = (pointB.x - pointA.x) / duration;
+    const stepY = (pointB.y - pointA.y) / duration;
+
+    const interval = 10;
+    let currentTime = 0;
+
+    const moveInterval = setInterval(() => {
+        currentTime += interval;
+
+        if (currentTime < duration)
+        {
+            pointA.x += stepX * interval;
+            pointA.y += stepY * interval;
+
+            hero.style.left = `${pointA.x}px`;
+            hero.style.top = `${pointA.y}px`;
+        } else {
+            clearInterval(moveInterval)
+            whereIsHero(e, places);
+        }
+    }, interval);
+}
+
+
+function whereIsHero(e, places)
+{
     const dimention_place = 50;
-    const hero_coor = hero.getBoundingClientRect();
+
+    const hero_next_coor_x = e.clientX;
+    const hero_next_coor_y = e.clientY;
 
     for (const place of places) {
         const place_coor = place.getBoundingClientRect();
-        const hero_next_coor_y = e.clientY;
-        const hero_next_coor_x = e.clientX;
-
-        hero.style.top = `${hero_next_coor_y}px`;
-        hero.style.left = `${hero_next_coor_x}px`;
 
         const place_central_x = place_coor.x + dimention_place;
         const place_central_y = place_coor.y + dimention_place;
@@ -27,7 +73,5 @@ function whereIsHero(e, hero, places) {
             return;
         }
     }
-    Array.from(places).forEach((p) => {
-        p.style.background = '#c3ff00';
-    });
+    Array.from(places).forEach((p) => { p.style.background = '#c3ff00'; });
 }
